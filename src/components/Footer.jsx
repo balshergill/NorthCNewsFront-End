4fa2044eaeaf9a8e19bf62dbf47c638ccdd82c69
balshergill <baldipkshergill@gmail.com>
2019-04-26 12:33:37 +0100

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../App.css";
 
 const Footer = () => {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <p>Copyright 2019 North C News All Rights Reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
